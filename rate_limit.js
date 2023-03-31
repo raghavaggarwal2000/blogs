5bd2f01ffbecd29f601b04315277ef5fe567761f
Raghav Aggarwal <aggarwalchirag2000@gmail.com>
2023-03-31 14:47:10 +0530

    max changed

--- rate_limit.js
+++ rate_limit.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-    windowMs: 1000 * 60,
-    max: 50,
+    windowMs: 1000 * 60,// 1min, ie 
+    max: 5,
     message: "Too many requests, please try again later."
 });
 
